02bd1b0e12b09240347ab0cacffd9ba3d5f3ca75
Dea Shafira A <89475793+deashfra@users.noreply.github.com>
2024-06-15 13:30:04 +0000

    Customize portfolio with matching color palette and serif font

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,13 @@
-// scripts.js
-console.log("Hi!");
+document.addEventListener('mousemove', function(event) {
+    const bubble = document.createElement('div');
+    bubble.className = 'bubble';
+    bubble.style.left = event.pageX + 'px';
+    bubble.style.top = event.pageY + 'px';
+    document.body.appendChild(bubble);
+  
+    // Hapus bubble setelah beberapa detik
+    setTimeout(function() {
+      bubble.remove();
+    }, 2000);
+  });
+  
